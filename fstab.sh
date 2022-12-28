e1c2f3a96777eeaca1ff732a6087d08016d3ea36
Matthew <matthewsorensenesqs@gmail.com>
2022-12-28 12:44:36 -0800

    create fstab

--- fstab.sh
+++ fstab.sh
@@ -11,16 +11,4 @@ if [ ! -d $DIR ]; then
 fi
 
 #create fstab
-fstabArray=(
-    "192.168.1.23:/mnt/md0/data      /library/data1  nfs     defaults        0       0"
-    "192.168.1.23:/mnt/md1/data2     /library/data2  nfs     defaults        0       0"
-    "192.168.1.23:/mnt/md2/data3     /library/data3  nfs     defaults        0       0"
-) 
-
-for i in ${fstabArray[@]}; do
-  if  grep -q "$i" "/etc/fstab" ; then
-         echo "$i exists" ; 
-  else
-         echo "$i does not exist" ; 
-  fi
-done
+wget -O https://raw.githubusercontent.com/hexeth/proxmox-starter/main/fstab -P /etc/
