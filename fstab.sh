405fbd54e9627332cd5d92be2595278b7baef7f7
Matthew <matthewsorensenesqs@gmail.com>
2022-12-28 11:49:16 -0800

    fix append

--- fstab.sh
+++ fstab.sh
@@ -7,6 +7,6 @@ mkdir /library/data2
 mkdir /library/data3
 
 #create fstab
-echo "192.168.1.23:/mnt/md0/data      /library/data1  nfs     defaults        0       0" | tee /etc/fstab
-echo "192.168.1.23:/mnt/md1/data2     /library/data2  nfs     defaults        0       0" | tee /etc/fstab
-echo "192.168.1.23:/mnt/md2/data3     /library/data3  nfs     defaults        0       0" | tee /etc/fstab
+echo "192.168.1.23:/mnt/md0/data      /library/data1  nfs     defaults        0       0" >> /etc/fstab
+echo "192.168.1.23:/mnt/md1/data2     /library/data2  nfs     defaults        0       0" >> /etc/fstab
+echo "192.168.1.23:/mnt/md2/data3     /library/data3  nfs     defaults        0       0" >> /etc/fstab
