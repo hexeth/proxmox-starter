a3f99133ddf37e66a4f123d97c2f326e71ed3945
Matthew <matthewsorensenesqs@gmail.com>
2022-12-28 14:47:51 -0800

    add chown

--- fstab.sh
+++ fstab.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#install nfs-common
+apt update
+apt install nfs-common git
+
+#install oh-my-bash
+bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
+
 #create dir structure
 DIR=/library
 if [ ! -d $DIR ]; then
@@ -7,10 +14,11 @@ if [ ! -d $DIR ]; then
     mkdir "/library/data1"
     mkdir "/library/data2"
     mkdir "/library/data3"
+    chown root:root /library -R
 fi
 
 #create fstab
 wget https://raw.githubusercontent.com/hexeth/proxmox-starter/main/fstab -O /etc/fstab
-
+chown root:root /etc/fstab
 #mount
 mount -a
