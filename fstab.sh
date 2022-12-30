35a2497ace4d4c06727f777d9dd02eef9288afd6
Matthew <matthewsorensenesqs@gmail.com>
2022-12-30 14:27:18 -0800

    fix width again

--- fstab.sh
+++ fstab.sh
@@ -66,11 +66,10 @@ function setup_git_and_bash {
 }
 
 # Show a dialog box with a list of options
-options=$(whiptail --scrolltext --title "Proxmox Starter" --checklist "What do you want to do?" 15 60 4 \
+options=$(whiptail --scrolltext --title "Proxmox Starter" --checklist "What do you want to do?" 20 80 10 \
   "Mount NFS drives" "Mount NFS drives and create fstab entries" OFF \
   "Install oh-my-bash" "Install git and oh-my-bash" OFF \
-  "Do both" "Mount NFS drives and install git and oh-my-bash" \ \
-  "--separate-output" 3>&1 1>&2 2>&3)
+  "Do both" "Mount NFS drives and install git and oh-my-bash" OFF 3>&1 1>&2 2>&3)
 
 # Check which options were selected
 if [[ $options == *"Mount NFS drives"* ]]; then
