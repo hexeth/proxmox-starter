af7f424c7f357094d1265b9f1d040e0786fd0704
Matthew <matthewsorensenesqs@gmail.com>
2022-12-30 15:31:44 -0800

    change to radiolist

--- fstab.sh
+++ fstab.sh
@@ -66,7 +66,7 @@ function setup_git_and_bash {
 }
 
 # Show a dialog box with a list of options
-options=$(whiptail --title "Proxmox Starter" --checklist "What do you want to do?" 15 60 4 \
+options=$(whiptail --title "Proxmox Starter" --radiolist "What do you want to do?" 15 60 4 \
   "Mount NFS drives" "Mount NFS drives" OFF \
   "Install oh-my-bash" "Install oh-my-bash" OFF \
   "Do both" "" \ ON --separate-output 3>&1 1>&2 2>&3)
